d90afb690a5af20c933f82ab9d0d6b4bbde65d2b
Christian Kirschberg <ckirschberg@gmail.com>
2024-10-30 13:36:36 +0100

    lifting state up

---
+++ src/components/AddTodo.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+// js - destructure - pakker attributter fra et objekt op.
+function AddTodo({todos, setTodos}) {
+    const [todo, setTodo] = useState(''); // what the user sees in the input type="text"
+    const [error, setError] = useState(null);
+
+    const handleOnTodoChange = (event) => {
+        console.log(event.target.value);
+        setTodo(event.target.value);
+    }
+
+    function handleAddTodo() {
+        if (todo === '') {
+            setError('Cannot add empty todo');
+        } else {
+            
+            // mutater det oprindelige array. NONONOOOO!!
+            // const newTodos = todos;
+            // const newTodo = { id: todos.length + 1, title: todo, completed: false };
+            // newTodos.push(newTodo);
+            // console.log(newTodos);
+            // setTodos(newTodos);
+
+            // opretter et nyt array og tilføjer det nye todo til array'et. YES!
+            const newTodoArray = [...todos, {id: todos.length + 1, title: todo, completed: false}]
+            setTodos(newTodoArray);
+        }
+    }
+
+
+
+    return (
+        <div>
+            <div>Add new todo</div>
+        <form>
+                <input 
+                    type="text" 
+                    placeholder="Add todo" 
+                    value={todo} 
+                    onChange={handleOnTodoChange} 
+                />
+                
+                {/* Conditional rendering: Vis kun p-tag hvis error ikke er null */}
+                { error !== null && <p style={{color: 'red'}}>{error}</p> }
+
+                <button type="button" onClick={handleAddTodo}>Add todo</button>
+            </form>
+
+        </div>
+    )
+} 
+
+export default AddTodo;
+
+export function add(a,b) {
+    return a + b;
+}
+export function philip(lunch) {
+    return lunch + ' with Philip';
+}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,13 @@
 import TodoItem from "./TodoItem";
 import { useState } from 'react';
+import AddTodo from './AddTodo'; 
+import { add, philip } from './AddTodo'; // import to functioner
+
 
 
 function TodoList() {
     // state: isChristianHappy='getter', setIsChristianHappy='setter'
     const [isChristianHappy, setIsChristianHappy] = useState(true);
-    const [todo, setTodo] = useState(''); // what the user sees in the input type="text"
-    const [error, setError] = useState(null);
     const [todos, setTodos] = useState([{id: 1, title: "Køb marsvin", completed: false}, 
         {id: 2, title: "Lav en todo list", completed: true}]);
 
@@ -18,43 +19,15 @@ function TodoList() {
         setIsChristianHappy(!isChristianHappy);
     }
 
-    const handleOnTodoChange = (event) => {
-        console.log(event.target.value);
-        setTodo(event.target.value);
-    }
-
-    function handleAddTodo() {
-        if (todo === '') {
-            setError('Cannot add empty todo');
-        } else {
-            
-            // mutater det oprindelige array. NONONOOOO!!
-            // const newTodos = todos;
-            // const newTodo = { id: todos.length + 1, title: todo, completed: false };
-            // newTodos.push(newTodo);
-            // console.log(newTodos);
-            // setTodos(newTodos);
-
-            // opretter et nyt array og tilføjer det nye todo til array'et. YES!
-            const newTodoArray = [...todos, {id: todos.length + 1, title: todo, completed: false}]
-            setTodos(newTodoArray);
-        }
-    }
-
+    
     return (
         <div>
             <h1>Todo List</h1>
 
-            <div>Add new todo</div>
-            <form>
-                <input type="text" placeholder="Add todo" value={todo} onChange={handleOnTodoChange}/>
-                
-                {/* Conditional rendering: Vis kun p-tag hvis error ikke er null */}
-                { error !== null && <p style={{color: 'red'}}>{error}</p> }
+            
+            <AddTodo todos={todos} setTodos={setTodos}/>
 
-                <button type="button" onClick={handleAddTodo}>Add todo</button>
-            </form>
-
+            
             <button onClick={handleChristianHappy}>Switch Christian Happy</button>
             Is Christian happy? { isChristianHappy.toString() }
 
